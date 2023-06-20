4849ad83b3a3c4d8e3113e114cf95cef9a7cf6c7
squiduu <wootj0104@gmail.com>
2023-06-20 11:48:32 +0900

    update naverpedia crawler

--- utils/crawler.py
+++ utils/crawler.py
@@ -1,157 +1,180 @@
-import argparse
 import requests
-from bs4 import BeautifulSoup
-from datetime import date, datetime, timedelta
-import json, os
+import json
 from tqdm import tqdm
-from utils.doc_processor import process_child_growth
+from bs4 import BeautifulSoup
+import re
 
-
-PEDIA_LINKS = {
-    "우리아이_성장백과": ("https://terms.naver.com/list.naver?cid=64672&categoryId=64672&so=date.dsc", 12), # &page=1~12 pages
-    "어린이백과_국어": ("https://terms.naver.com/list.naver?cid=47303&categoryId=47303", 115),
-    "어린이백과_영어": ("https://terms.naver.com/list.naver?cid=47304&categoryId=47304", 8),
-    "어린이백과_사회": ("https://terms.naver.com/list.naver?cid=47305&categoryId=47305", 87),
-    "어린이백과_한국사": ("https://terms.naver.com/list.naver?cid=47306&categoryId=47306", 159),
-    "어린이백과_세계사": ("https://terms.naver.com/list.naver?cid=47307&categoryId=47307", 38),
-    "어린이백과_수학" : ("https://terms.naver.com/list.naver?cid=47308&categoryId=47308", 20),
-    "어린이백과_과학" : ("https://terms.naver.com/list.naver?cid=47308&categoryId=47309", 155),
-    "어린이백과_예체능" : ("https://terms.naver.com/list.naver?cid=47308&categoryId=47310", 36),
-    "어린이백과_시사/논술" : ("https://terms.naver.com/list.naver?cid=47308&categoryId=47311", 7),
-    "어린이백과_문학탐구" : ("https://terms.naver.com/list.naver?cid=47308&categoryId=47314", 14),
-    "어린이백과_세계탐구" : ("https://terms.naver.com/list.naver?cid=47308&categoryId=47315", 29),
-    "어린이백과_과학탐구" : ("https://terms.naver.com/list.naver?cid=47308&categoryId=47316", 27),
-    "어린이백과_인물탐구" : ("https://terms.naver.com/list.naver?cid=47308&categoryId=47317", 43),
-    "어린이백과_유적/유물탐구" : ("https://terms.naver.com/list.naver?cid=47308&categoryId=47318", 6),
-    "어린이백과_동식물" : ("https://terms.naver.com/list.naver?cid=47308&categoryId=55616", 93),
-    "어린이백과_지식e" : ("https://terms.naver.com/list.naver?cid=47308&categoryId=58436", -1),
-    "어린이백과_천재학습백과" : ("https://terms.naver.com/list.naver?cid=47308&categoryId=58582", 535),
-    "어린이백과_소프트웨어" : ("https://terms.naver.com/list.naver?cid=47308&categoryId=59930", 10),
-    "대한민국_구석구석": ("https://terms.naver.com/list.naver?cid=42856&categoryId=42856&so=date.dsc", 952),
-    # "학생백과_국어/문학": ("https://terms.naver.com/list.naver?cid=47319&categoryId=47319", 467),
-    # "학생백과_한자": ("https://terms.naver.com/list.naver?cid=47320&categoryId=47320", 36),
-    # "학생백과_사회": ("https://terms.naver.com/list.naver?cid=47321&categoryId=47321", 722),
-    # "학생백과_한국사": ("https://terms.naver.com/list.naver?cid=47322&categoryId=47322",),
-    # "학생백과_세계사": "https://terms.naver.com/list.naver?cid=47323&categoryId=47323",
-    # "학생백과_수학": "https://terms.naver.com/list.naver?cid=47324&categoryId=47324",
-    # "학생백과_과학": "https://terms.naver.com/list.naver?cid=47325&categoryId=47325",
-    # "학생백과_기술/가정": "https://terms.naver.com/list.naver?cid=47326&categoryId=47326",
-    # "학생백과_예체능": "https://terms.naver.com/list.naver?cid=47327&categoryId=47327",
-    # "학생백과_인물": "https://terms.naver.com/list.naver?cid=47328&categoryId=47328",
-    # "학생백과_시사/논술": "https://terms.naver.com/list.naver?cid=47329&categoryId=47329",
-    # "학생백과_동식물": "https://terms.naver.com/list.naver?cid=55619&categoryId=55619",
-    # "학생백과_소프트웨어": "https://terms.naver.com/list.naver?cid=59940&categoryId=59940",   
+BASE_URLS = {
+    "우리아이_성장백과": "https://terms.naver.com/list.naver?cid=64672&categoryId=64672&so=date.dsc",
+    "어린이백과_국어": "https://terms.naver.com/list.naver?cid=47303&categoryId=47303",
+    "어린이백과_영어": "https://terms.naver.com/list.naver?cid=47304&categoryId=47304",
+    "어린이백과_사회": "https://terms.naver.com/list.naver?cid=47305&categoryId=47305",
+    "어린이백과_한국사": "https://terms.naver.com/list.naver?cid=47306&categoryId=47306",
+    "어린이백과_세계사": "https://terms.naver.com/list.naver?cid=47307&categoryId=47307",
+    "어린이백과_수학": "https://terms.naver.com/list.naver?cid=47308&categoryId=47308",
+    "어린이백과_과학": "https://terms.naver.com/list.naver?cid=47308&categoryId=47309",
+    "어린이백과_예체능": "https://terms.naver.com/list.naver?cid=47308&categoryId=47310",
+    "어린이백과_시사/논술": "https://terms.naver.com/list.naver?cid=47308&categoryId=47311",
+    "어린이백과_문학탐구": "https://terms.naver.com/list.naver?cid=47308&categoryId=47314",
+    "어린이백과_세계탐구": "https://terms.naver.com/list.naver?cid=47308&categoryId=47315",
+    "어린이백과_과학탐구": "https://terms.naver.com/list.naver?cid=47308&categoryId=47316",
+    "어린이백과_인물탐구": "https://terms.naver.com/list.naver?cid=47308&categoryId=47317",
+    "어린이백과_유적/유물탐구": "https://terms.naver.com/list.naver?cid=47308&categoryId=47318",
+    "어린이백과_동식물": "https://terms.naver.com/list.naver?cid=47308&categoryId=55616",
+    "어린이백과_지식e": "https://terms.naver.com/list.naver?cid=47308&categoryId=58436",
+    "어린이백과_천재학습백과": "https://terms.naver.com/list.naver?cid=47308&categoryId=58582",
+    "어린이백과_소프트웨어": "https://terms.naver.com/list.naver?cid=47308&categoryId=59930",
+    # "대한민국_구석구석": "https://terms.naver.com/list.naver?cid=42856&categoryId=42856&so=date.dsc",
 }
+ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
 
 
-class NaverpediaCrawler(object):
-    def __init__(self, base_links):
-        self.base_links = base_links
-    
-    def parse_content_links(self, base_link, num_pages):
-        """Parse content links from base link and number of pages"""
-        
-        page_links = [base_link + f"&page={i}" for i in range(1, num_pages+1)]
-        content_links = []
-        
-        num_pages = len(page_links)
-        for page_link in tqdm(page_links, total=num_pages):
-            res = requests.get(page_link)
-            res.raise_for_status()
-            res.encoding = None
-            html = res.text
-            soup = BeautifulSoup(html, 'html.parser')
-            content_list = soup.find('ul', {'class': 'content_list'}).findAll('div', {'class': 'image_area'})
-            for div in content_list:
-                link = div.find('a')
-                real_link = 'https://terms.naver.com' + link.get('href')
-                content_links.append(real_link)
-        
-        return content_links
-    
-    def get_all_content_links(self, save_dir=None):
-        """Get all content links from base links"""
-        
-        results = {}
-        for name, base_link in self.base_links.items():
-            if base_link[1]==-1:
-                continue
-            print("Connect", name)
-            content_links = self.parse_content_links(base_link[0], base_link[1])
-            results[name] = content_links
-        
-        if save_dir is not None:
-            with open(os.path.join(save_dir, "all_content_links.json"), "w") as fout:
-                json.dump(results, fout, indent=2, ensure_ascii=False)
-        
-        return results
-            
-    def load_all_content_links(self, cache_path):
-        with open(cache_path, "r") as fin:
-            link_dict = json.load(fin)
-            
-        return link_dict
-    
-    def scrap_contents(self, links, process_func):
-        """Scrap contents from links"""
-        
-        contents = []
-        for link in tqdm(links, total=len(links), desc="Scraping"):
-            res = requests.get(link)
-            res.raise_for_status()
-            res.encoding = None
-            html = res.text
-            soup = BeautifulSoup(html, "html.parser")
-            titlebox = soup.find("div", {"class": "headword_title"})
-            title = titlebox.find("h2", {"class": "headword"}).text
-            subtitle = titlebox.find("p", {"class": "desc"}).text
-            main_texts = soup.findAll("p", {"class": "se_textarea"})
-            doc = []
-            for seg in main_texts:
-                doc.extend(seg.findAll("span"))
-            
-            # preprocess document
-            doc = process_func(doc)
-            doc = "\n".join(doc)
-            
-            contents.append({
-                "title": title, "subtitle": subtitle,
-                "doc": doc, "url": link
-            })
-        
-        return contents
-            
-    def crawl(self, save_dir, cache_path=None):
-        """Crawl all contents from base links"""
-        
-        if cache_path is not None:
-            link_dict = self.load_all_content_links(cache_path)
+def get_page_len(base_url: str) -> int:
+    final_page = 1
+    while True:
+        page_num = 1
+
+        url = base_url + "&page=" + str(page_num)
+        response = requests.get(url, headers={"User-Agent": ua})
+
+        page = response.text
+        soup = BeautifulSoup(page, "html.parser")
+        page_list = soup.select("div#paginate > a")
+        last_page_num = int(page_list[-1]["href"].split("=")[-1])
+
+        if last_page_num == final_page:
+            break
+
+        final_page = last_page_num
+
+    return final_page
+
+
+def get_contents(url):
+    base = "https://m.terms.naver.com"
+    url = base + url
+    response = requests.get(url, headers={"User-Agent": ua})
+
+    page = response.text
+    soup = BeautifulSoup(page, "html.parser")
+    contents = soup.find_all("div", {"class": "atomic-block"})
+
+    result = []
+
+    for content in contents:
+        if "이미지 갤러리 가기" in content.text:
+            continue
+        elif content.find("h2"):
+            texts = re.sub("\n+", "\n", content.text)
+            texts = re.sub("\s+", " ", texts)
+            result.append("## " + texts + "\n")
         else:
-            link_dict = self.get_all_content_links(save_dir)
-        
-        all_contents = {}
-        for name, links in link_dict.items():
-            print(name)
-            if name=="우리아이_성장백과":
-                all_contents[name] = self.scrap_contents(links, process_child_growth)
-            else:
-                break
-                # raise NotImplementedError
-        
-        with open(os.path.join(save_dir, "parsed_contents.json"), "w") as fout:
-            json.dump(all_contents, fout, indent=2, ensure_ascii=False)
-        
-        return all_contents
-            
-        
+            texts = re.sub("\n+", "\n", content.text)
+            texts = re.sub("\s+", " ", texts)
+            result.append(texts)
 
-if __name__=="__main__":
-    parser = argparse.ArgumentParser("Crawler")
-    parser.add_argument("--save_dir", type=str, default="data/web_crawled")
-    args = parser.parse_args()
-    
-    os.makedirs(args.save_dir, exist_ok=True)
-    
-    crawler = NaverpediaCrawler(PEDIA_LINKS)
-    crawler.crawl(args.save_dir, cache_path="data/web_crawled/all_content_links.json")
-    
+    return "".join(result)
+
+
+def parse_e(url):
+    base = "https://m.terms.naver.com"
+    url = base + url
+    response = requests.get(url, headers={"User-Agent": ua})
+
+    page = response.text
+    soup = BeautifulSoup(page, "html.parser")
+    contents = soup.find_all("div", {"class": "atomic-block"})
+
+    result = []
+
+    title = soup.select("h1 > span")
+    title = " ".join([t.text.strip() for t in title])
+
+    for content in contents:
+        if "이미지 갤러리 가기" in content.text:
+            continue
+        elif content.find("strong"):
+            headding = content.select("strong")
+            headding = [h.text.strip() for h in headding]
+            num = len(headding)
+            headding = "\n".join(headding)
+            result.append("## " + headding + "\n")
+            texts = re.sub(f"\n+", f"\n", content.text[len(headding) + num :].strip())
+            texts = re.sub(f"\s+", " ", texts)
+            result.append(texts)
+        else:
+            texts = re.sub(f"\n+", f"\n", content.text.strip())
+            texts = re.sub(f"\s+", " ", texts)
+            result.append(texts)
+
+    return "".join(result), title
+
+
+def save_json(data):
+    with open("icecream.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+def get_e_contents():
+    base_url = "https://terms.naver.com/list.naver?cid=58436&categoryId=58436"
+    res = requests.get(base_url)
+    soup = BeautifulSoup(res.text, "html.parser")
+    url_infos = soup.select(".contents_list > .contents_sub")[0]
+
+    urls = [tags["href"] for tags in url_infos.select("a") if tags["href"] != "#"]
+
+    total_results = []
+
+    for url in urls:
+        content, title = parse_e(url)
+        results = {}
+
+        content_url = url
+
+        results["category"] = "어린이백과_지식e"
+        results["title"] = title
+        results["contents"] = content
+        results["url"] = content_url
+
+        total_results.append(results)
+    return total_results
+
+
+def main():
+    total_results = []
+
+    for cat_name, base_url in tqdm(BASE_URLS.items()):
+        if cat_name == "어린이백과_지식e":
+            e_results = get_e_contents()
+            total_results.extend(e_results)
+            continue
+        page_len = get_page_len(base_url)
+        for i in range(page_len + 1):
+            url = base_url + "&page=" + str(i)
+            response = requests.get(url, headers={"User-Agent": ua})
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            content_list = content_list = soup.select("ul.content_list > li")
+
+            for c in content_list:
+                results = {}
+                try:
+                    title = c.find("img")["alt"]
+                except:
+                    title = c.find("a").text
+
+                content_url = c.find("a")["href"]
+                contents = get_contents(content_url)
+
+                results["category"] = cat_name
+                results["title"] = title
+                results["contents"] = contents
+                results["url"] = content_url
+
+                total_results.append(results)
+
+    save_json(total_results)
+
+
+if __name__ == "__main__":
+    main()
--- utils/doc_processor.py
+++ utils/doc_processor.py
@@ -1,45 +1,51 @@
 import re
+import json
+import copy
 
-def process_child_growth(doc):
-    """우리아이 성장백과 document preprocessing"""
-    
-    parsed = []
-    heading = False
-    concat = False
-    try:
-        for span in doc:
-            class_names = span.attrs.get("class")
-            if class_names:
-                if "se_fs_T4" in class_names:
-                    # ignore and append to next line
-                    concat = True
-                    if parsed:
-                        parsed[-1] += span.text
-                    else:
-                        parsed.append(span.text)
-                    
-                elif not heading and "se_fs_T2" in class_names:
-                    parsed.append("### " + span.text)
-                    heading = True
-                else:
-                    if re.compile(r"[가-힣]+").match(span.text):
-                        parsed[-1] += span.text
-            else:
-                heading = False
-                # if span.text is composed of korean characters'
-                if re.compile(r"[가-힣]+").match(span.text):
-                    if concat:
-                        parsed[-1] += span.text
-                        concat = False
-                    else:
-                        parsed.append(span.text)
-                elif re.compile(r"[1-9].").match(span.text):
-                    concat = True
-                    parsed.append(span.text)
-                    
-    except Exception as e:
-        print(parsed)
-        print(heading)
-        print(span)
-        
-    return parsed
+
+def remove_superfluous_char(doc):
+    # Remove video tag
+    doc = re.sub(r"(addLoadEvent)[^\n]+;", " ", doc)
+
+    # Remove links
+    doc = re.sub(r"((http)([s:/]{3,4})?)?[a-z0-9]+([\.][a-z0-9]+)+[\S]+", " ", doc)
+    # Remove email
+    doc = re.sub(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)", " ", doc)
+    # Remove special char
+    doc = re.sub(r"[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9\s\?\!\.\-()$%+=\'\":]", " ", doc)
+    # Remove tags
+    doc = re.sub(r"<(\/)?[a-z]+>", "", doc)
+    # Reduce repetition
+    doc = re.sub(r"([^가-힣a-zA-Z0-9])\1{1,}", r"\1", doc)
+    doc = re.sub(r"([^0-9])\1{3,}", r"\1\1\1", doc)
+
+    # Remove chosung
+    doc = re.sub(r"[ㄱ-ㅎㅏ-ㅣ]+", " ", doc)
+    # Remove spaces
+    doc = re.sub(r"[ ]{2,}", " ", doc)
+    doc = re.sub(r"(\s\.\s)", ". ", doc)
+    doc = doc.replace(".. ", "")
+    doc = doc.replace(".?", "?")
+    doc = doc.replace(".!", "!")
+
+    # Remove line alignments
+    doc = re.sub(r"([\s])\1{1,}", " ", doc)
+    doc = re.sub(r"[\s]+", " ", doc)
+
+    return doc.strip()
+
+
+def main():
+    with open(file="./icecream.json", mode="r") as f:
+        data = json.load(f)
+
+    preprocessed = copy.deepcopy(data)
+    for i in range(len(data)):
+        preprocessed[i]["contents"] = remove_superfluous_char(data[i]["contents"])
+
+    with open(file="./preprocessed.json", mode="w") as f:
+        json.dump(obj=preprocessed, fp=f, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    main()
